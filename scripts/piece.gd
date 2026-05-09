50da0edc8f01a07397bb9818d7017062268ba2d9
Evan Johnson <johnsonev207@gmail.com>
2026-05-08 22:05:17 -0700

    Palette swap for pieces

--- scripts/piece.gd
+++ scripts/piece.gd
@@ -1,9 +1,24 @@
+@tool
+
 class_name Piece
 
 extends Node2D
 
-@export var is_player: bool = false
+@export var is_player: bool = false:
+	set(new_value):
+		is_player = new_value
+		_set_palette()
 
 
 func _can_reach(_coordinate: Vector2i) -> bool:
 	return false
+
+
+func _ready() -> void:
+	_set_palette()
+
+
+func _set_palette() -> void:
+	var sprite: Sprite2D = $Sprite2D
+	var shader_material: ShaderMaterial = sprite.material
+	shader_material.set_shader_parameter("is_enabled", !is_player)
